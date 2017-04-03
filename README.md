d4b72af25160320b37bad2f324e20628728694ab
gomi_ningen <korun24276@gmail.com>
2017-04-03 21:52:18 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Xamarin.iOS Samples
+
+
+
---
+++ XamarinSample/ViewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using UIKit;
+
+namespace XamarinSample
+{
+    public class ViewController : UIViewController
+    {
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            View.BackgroundColor = UIColor.Orange;
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
