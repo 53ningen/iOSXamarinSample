31834ef6e29a7f17a07843684723fc34201b67d0
gomi_ningen <korun24276@gmail.com>
2017-04-12 18:07:23 +0900

    Add XamarinSample.MvvmCross.ViewModels Project(PCL)

--- XamarinSample/Main/MainViewController.cs
+++ XamarinSample/Main/MainViewController.cs
@@ -17,9 +17,10 @@ namespace XamarinSample
             const int width = 200;
             const int height = 40;
             var button = UIButton.FromType(UIButtonType.System);
-            button.Frame = new CGRect((View.Bounds.Width - width) / 2, (View.Bounds.Height - height) / 2, width, height);
+            button.WidthAnchor.ConstraintEqualTo(200).Active = true;
+            button.HeightAnchor.ConstraintEqualTo(40).Active = true;
+            //button.Frame = new CGRect((View.Bounds.Width - width) / 2, (View.Bounds.Height - height) / 2, width, height);
             button.SetTitle("UIPageView Sample", UIControlState.Normal);
-
             button.TouchDown += (sender, e) =>
             {
                 var vc = new SamplePageController();
@@ -27,7 +28,16 @@ namespace XamarinSample
                 vc.EdgesForExtendedLayout = UIRectEdge.None;
                 NavigationController?.PushViewController(vc, true);
             };
-            View.AddSubview(button);
+            var stackView = new UIStackView();
+            stackView.Axis = UILayoutConstraintAxis.Vertical;
+            stackView.Distribution = UIStackViewDistribution.Fill;
+            stackView.Alignment = UIStackViewAlignment.Fill;
+            stackView.Spacing = 0;
+            stackView.AddArrangedSubview(button);
+            View.AddSubview(stackView);
+
+            stackView.CenterXAnchor.ConstraintEqualTo(View.CenterXAnchor).Active = true;
+            stackView.CenterYAnchor.ConstraintEqualTo(View.CenterYAnchor).Active = true;
         }
 
         public override void DidReceiveMemoryWarning()
